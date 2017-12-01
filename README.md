265a4d398668c0da56269ed73572bc29c4e1c87e
Dave McKenzie <davidm@Daves-iMac.local>
2017-12-01 13:26:36 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # telebagger
-Simple Telegram to Discord one-way relay service
+Simple Telegram to Discord one-way relay service. Uses the [Discord-Webhooks](https://github.com/kyb3r/Discord-Hooks) and [Telethon](https://github.com/LonamiWebs/Telethon) libraries.
+
+Monitors a specified Telegram channel and relays any messages received in that channel to a Discord webhook. Designed to run as a human user for channels where bots are not permitted.
+
+## Installation
+```
+pip install telethon
+```
+Then copy config.example.json and set the fields accordingly.
+* discord.url - Should be set to a Discord webhook URL. Your Discord admin will need to create this.
+* telegram.api_id & api_hash - [These can be obtained from Telegram](https://core.telegram.org/api/obtaining_api_id)
+* telegram.phone - Should be the phone number of the user you wish to connect as (remember to include country code)
+* telegram.channel_id - This is the ID of the channel you are monitoring. If you don't have this, you will be presented with a list of available channels on first run of the script.
+
+Once config is set up, simply run:
+```
+python telebagger.py
+```
---
+++ telebagger.py
@@ -0,0 +1,77 @@
+######
+#
+# Telebagger - Simple Telegram to Discord relay service
+#
+######
+
+from discord_hooks import Webhook
+from telethon import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import UpdateShortMessage, UpdateNewChannelMessage, PeerUser, PeerChat, PeerChannel, InputPeerEmpty, Channel
+from time import sleep
+import json
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('telebagger')
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+try:
+    url = config['discord']['url']
+    api_id = config['telegram']['api_id']
+    api_hash = config['telegram']['api_hash']
+    phone = config['telegram']['phone']
+    channel_id = config['telegram']['channel_id']
+except:
+    logger.error('Error processing config file')
+
+print('Connecting to Telegram...')
+
+tclient = TelegramClient('session_name', api_id, api_hash, update_workers=4, spawn_read_thread=False)
+tclient.connect()
+if not tclient.is_user_authorized():
+    tclient.send_code_request(phone)
+    myself = tclient.sign_in(phone, input('Enter code: '))
+
+def callback(update):
+    if type(update) is UpdateNewChannelMessage:
+        try:
+            print(update)
+            print("Channel ID: {}".format(update.message.to_id.channel_id))
+            if update.message.to_id.channel_id == channel_id:
+                msgText = "*{}*: {}".format(channel_name, update.message.message)
+                msg = Webhook(url,msg=update.message.message)
+                msg.post()
+            else:
+                logger.info('ignoring message to wrong channel')
+        except:
+            logger.info('no message')
+
+#msg = Webhook(url,msg="Telebagger ready to bag yo telegrams")
+#msg.post()
+
+tclient.add_update_handler(callback)
+
+last_date = None
+chunk_size = 20
+result = tclient(GetDialogsRequest(
+                 offset_date=last_date,
+                 offset_id=0,
+                 offset_peer=InputPeerEmpty(),
+                 limit=chunk_size
+             ))
+print("\nAvailable Channels:")
+for p in result.chats:
+    if type(p) is Channel:
+        print(str(p.id)+": "+p.title)
+        if p.id == channel_id:
+            channel_name = p.title
+
+try:
+    logger.info("\nListening for messages from channel '{}' with ID '{}'".format(channel_name,channel_id))
+except:
+    logger.error("Whoops! Couldn't find channel ID '{}'".format(channel_id))
+
+tclient.idle()
+tclient.disconnect()
